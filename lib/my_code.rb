8b25a01ff0d4eb29c815db11b25f769f7a998ba6
Kimberly Bone <k_bone@uncg.edu>
2019-07-31 03:02:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,47 @@
-# Your Code Here
+def map(source_array)
+  array = []
+  i = 0
+  while i < source_array.length 
+    array << yield(source_array[i])
+    i += 1
+  end
+  return array
+end 
+
+
+def reduce(source_array, starting_point = nil)
+  i = 0
+  if starting_point == nil 
+    starting_point = source_array[0]
+    i += 1
+  end 
+  while i < source_array.length
+    starting_point = yield(starting_point, source_array[i])
+    i += 1
+  end
+    starting_point
+end 
+#   if starting_point == nil 
+#     source_array.each do |num|
+#       total += num
+#     end 
+#   else 
+#     total = starting_point + source_array.sum
+#   end 
+#   total
+# end 
+#   if starting_point != nil
+#     i = 1
+#       total = starting_point + source_array.sum
+#     else 
+#       total = source_array.sum 
+#   end 
+#   i = 0 
+#   while i < source_array.length
+#     element = source_array[i]
+#     if element == false 
+#       return false 
+#     end 
+#     i += 1 
+#   end 
+# end 
